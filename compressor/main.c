22718bef40ec9603119cabc40c04cbd96329379a
TT-392 <tt-392@protonmail.com>
2021-04-19 23:16:03 +0200

    sorta first functional algorythm, no optimizations, just the smalles possible partial updates

--- compressor/main.c
+++ compressor/main.c
@@ -3,111 +3,136 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+
 #include "fileHandling.h"
 
-uint8_t bitmap[240*30] = {};
-bool currentScreen[240][240] = {};
-bool newScreen[240][240] = {};
+#define frameWidth 56
+#define frameHeight 49
+
+uint8_t bitmap[frameHeight*(frameWidth/8)] = {};
+uint8_t startFrame[frameHeight*(frameWidth/8)] = {};
+bool frameBeingOverwritten[frameWidth][frameHeight] = {};
+bool newFrame[frameWidth][frameHeight] = {};
 
 int main() {
-    struct dataBlock testData = {0, 0, 239, 239, 0, bitmap};
     FILE *file;
     file = fopen("output","wb");
 
 
 
-    for (int i = 1; i < 6572; i++) {
+    for (int i = 1; i < 3; i++) {
         FILE *input;
         char buffer[50];
 
-        sprintf(buffer, "../video/30fps/apple%05d.png.mono", i);
+        //sprintf(buffer, "../video/30fps/apple%05d.png.mono", i);
+        sprintf(buffer, "../frame%i.mono", i);
 
         printf(buffer);
         printf("\n");
 
         input = fopen(buffer, "rb");
-        for (int y = 0; y < 240; y++) {
-            for (int x = 0; x < 30; x++) {
+        for (int y = 0; y < frameHeight; y++) {
+            for (int x = 0; x < frameWidth/8; x++) {
                 uint8_t byte = fgetc(input);
                 for (int b = 0; b < 8; b++) {
-                    newScreen[x*8 + b][y] = (byte >> b) & 1;
+                    newFrame[x*8 + b][y] = (byte >> b) & 1;
                 }
             }
         }
 
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = 0;
-        for (int y = 0; y < 240; y++) {
-            for (int x = 0; x < 240; x++) {
-                if (currentScreen[x][y] != newScreen[x][y]) {
-                    x1 = x;
-                    y1 = y;
+        if (i == 1) {
+            
+        } else {
+            bool stuffChanging = 1;
+            while (stuffChanging) {
+                struct dataBlock block = {0};
+                block.x1 = 0;
+                block.y1 = 0;
+                block.x2 = 0;
+                block.y2 = 0;
+                block.bitmap = bitmap;
 
-                    int diffPixels[4] = {0};
-                    int samePixels[4] = {0};
-                    bool edge[4] = {0};
-
-                    for (int direction = 0; direction < 4; direction++) {
-
-                        switch (direction) {
-                            case 0:
-                                if (x1 + x2 < 240 - 1) {
-                                    for (int i = 0; i < (y2 + 1); i++) {
-                                        if (currentScreen[x1 + x2 + 1][y1 + i] != newScreen[x1 + x2 + 1][y1 + i])
-                                            diffPixels[direction]++;
-                                        else
-                                            samePixels[direction]++;
-                                    }
-                                } else{
-                                    edge[direction] = 1;
-                                }
-                                break;
-                            case 1:
-                                if (x1 - 1 >= 0) {
-                                    for (int i = 0; i < (y2 + 1); i++) {
-                                        if (currentScreen[x1 - 1][y1 + i] != newScreen[x1 - 1][y1 + i])
-                                            diffPixels[direction]++;
-                                        else
-                                            samePixels[direction]++;
-                                    }
-                                } else{
-                                    edge[direction] = 1;
-                                }
-                                break;
-                            case 2:
-                                if (y1 + y2 < 240 - 1) {
-                                    for (int i = 0; i < (x2 + 1); i++) {
-                                        if (currentScreen[y1 + y2 + 1][x1 + i] != newScreen[y1 + y2 + 1][x1 + i])
-                                            diffPixels[direction]++;
-                                        else
-                                            samePixels[direction]++;
-                                    }
-                                } else{
-                                    edge[direction] = 1;
-                                }
-                                break;
-                            case 3:
-                                if (y1 - 1 >= 0) {
-                                    for (int i = 0; i < (x2 + 1); i++) {
-                                        if (currentScreen[y1 - 1][x1 + i] != newScreen[y1 - 1][x1 + i])
-                                            diffPixels[direction]++;
-                                        else
-                                            samePixels[direction]++;
-                                    }
-                                } else{
-                                    edge[direction] = 1;
-                                }
-                                break;
+                stuffChanging = 0;
+                for (int y = 0; y < frameHeight; y++) {
+                    for (int x = 0; x < frameWidth; x++) {
+                        if (newFrame[x][y] != frameBeingOverwritten[x][y]) {
+                            block.x1 = x;
+                            block.y1 = y;
+                            stuffChanging = 1;
+                            goto nestedBreak;
                         }
                     }
                 }
+                nestedBreak:
+
+                while (newFrame[block.x1+block.x2+1][block.y1] != frameBeingOverwritten[block.x1+block.x2+1][block.y1]) {
+                    block.x2++;
+                }
+
+                int samePixels = 0;
+
+                while (samePixels == 0) {
+                    block.y2++;
+                    for (int x = 0; x < block.x2+1; x++) {
+                        if (newFrame[block.x1+x][block.y1+block.y2] == frameBeingOverwritten[block.x1+x][block.y1+block.y2]) {
+                            samePixels++;
+                        }
+                    }
+                }
+                block.y2--;
+
+
+                getchar();
+                ////// render //////
+                for (int y = 0; y < frameHeight; y++) {
+                    for (int x = 0; x < frameWidth; x++) {
+                        bool selected = 0;
+                        if (x >= block.x1 && x <= block.x1+block.x2 && y >= block.y1 && y <= block.y1+block.y2)
+                            selected = 1;
+
+                        if (newFrame[x][y]) {
+                            if (!selected) {
+                                if (frameBeingOverwritten[x][y])
+                                    printf("  ");
+                                else // changed
+                                    printf("◍ ");
+                            } else {
+                                if (frameBeingOverwritten[x][y])
+                                    printf("□ ");
+                                else // changed
+                                    printf("▥ ");
+                            }
+                        } else {
+                            if (!selected) {
+                                if (frameBeingOverwritten[x][y]) // changed
+                                    printf("○ ");
+                                else 
+                                    printf("  ");
+                            } else {
+                                if (frameBeingOverwritten[x][y]) // changed
+                                    printf("▤ ");
+                                else 
+                                    printf("□ ");
+                            }
+                        }
+                        if (selected) {
+                            newFrame[x][y] = frameBeingOverwritten[x][y];
+                        }
+                    }
+                    printf("\n");
+                }
+                if (stuffChanging)
+                    writeBlock(block, file);
+            }
+        }
+
+        for (int y = 0; y < frameHeight; y++) {
+            for (int x = 0; x < frameWidth; x++) {
+                frameBeingOverwritten[x][y] = newFrame[x][y];
             }
         }
 
         fclose(input);
-        writeBlock(testData, file);
     }
 
 
